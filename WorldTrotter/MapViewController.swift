3452a57ff296e1354ea983834758ace7aa42fdf2
stephen <stephen.a.atwood@gmail.com>
2016-03-14 16:41:07 -0500

    Completed instructions from Chapter 4

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Stephen Atwood on 3/14/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        // Always call the super implementation of viewDidLoad
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view")
+    }
+}
